3cd67ce8c0c16bbe7a52b4d92b1ed3fd98a2e5e5
Abdul kader <abdulkadir14015@gmail.com>
2019-10-17 16:46:02 +0600

    Add files via upload

---
+++ Gateway/StudentGateway.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tuto7.Models;
+using System.Data;
+using System.Data.SqlClient;
+namespace Tuto7.Gateway
+{
+    public class StudentGateway
+    {
+        public List<Student> GetStudents()
+        {
+
+            SqlConnection connection = new SqlConnection(@"Server=DESKTOP-059A10P\SQLEXPRESS;Database=ASPCRUD;Integrated Security=true;");
+
+            
+            string query = "SELECT * FROM Contact3";
+            //SqlCommand cmd = new SqlCommand(query, connection);
+           // connection.Open();
+
+            SqlCommand command =new SqlCommand(query, connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+        
+            List<Student> students = new List<Student>();
+            while(reader.Read())
+            {
+
+                Student student = new Student();
+                student.Name = reader["Name"].ToString();
+                student.Mobile = reader["Mobile"].ToString();
+                students.Add(student); // students collection e sokal stdn add korsi
+            }
+            connection.Close();
+            return students;
+
+
+
+        }
+
+        private void ExecuteReader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Manager/StudentManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tuto7.Gateway;
+using Tuto7.Models;
+
+namespace Tuto7.Manager
+{
+    public class StudentManager
+    {
+
+        StudentGateway studentGateway = new StudentGateway();
+
+        public List<Student>GetStudents()
+        {
+
+            return studentGateway.GetStudents();
+        }
+
+
+    }
+}
